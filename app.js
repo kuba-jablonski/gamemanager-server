2572c2bcb96e31916562dea523b55ec09bec50fe
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2020-01-20 01:21:38 +0100

    enable cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const gameRouter = require("./routes/gameRoutes");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/games", gameRouter);
