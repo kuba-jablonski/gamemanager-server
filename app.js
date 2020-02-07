b8d17557783443fa534e51752daf596c8379ce04
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2020-02-07 13:09:52 +0100

    add user router

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const gameRouter = require("./routes/gameRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/games", gameRouter);
+app.use("/api/v1/users", userRouter);
 
 module.exports = app;
