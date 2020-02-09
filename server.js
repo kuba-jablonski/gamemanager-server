00799d07b5be26d93c1d6bf899ef980009d7726c
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2020-02-09 19:14:26 +0100

    handle server errors

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+process.on("uncaughtException", err => {
+  console.log("UNCAUGHT EXCEPTION!!! Shutting down...");
+  console.log(err);
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
@@ -16,6 +22,14 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on("unhandledRejection", err => {
+  console.log("UNHANDLED REJECTION!!! Shutting down...");
+  console.log(err);
+  server.close(() => {
+    process.exit(1);
+  });
 });
