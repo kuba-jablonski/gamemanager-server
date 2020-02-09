2c78702305af6f566296fe51e0583fc52af561d4
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2020-02-09 14:25:41 +0100

    hide error details in production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,31 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      message: err.message
+    });
+  } else {
+    console.error("ERROR: ", err);
+
+    res.status(500).json({
+      message: "Something went very wrong!"
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
 
-  res.status(err.statusCode).json({
-    message: err.message
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrorProd(err, res);
+  }
 };
