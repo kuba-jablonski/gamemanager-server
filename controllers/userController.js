bab195ab1a13d927357b7067f4c59a4f2366df94
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2020-02-25 17:10:28 +0100

    remove import'

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
-const AppError = require("../utils/appError");
+// const AppError = require("../utils/appError");
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
