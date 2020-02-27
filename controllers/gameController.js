dd734489df70d8285964cbf6ae9ec8bfd8838d71
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2020-02-27 23:09:26 +0100

    remove Backlog related files

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,67 +1,6 @@
 const catchAsync = require("../utils/catchAsync");
-// const Backlog = require("../models/backlogModel");
 // const AppError = require("../utils/appError");
 const Game = require("../models/gameModel");
-
-// exports.createGame = catchAsync(async (req, res, next) => {
-//   const backlog = await Backlog.findOneAndUpdate(
-//     { owner: req.user._id, "items.apiId": { $ne: req.body.apiId } },
-//     { $push: { items: req.body } },
-//     { new: true, runValidators: true }
-//   );
-
-//   if (!backlog) return next(new AppError("You already added this game.", 400));
-
-//   res.status(200).json({ backlog });
-// });
-
-// exports.updateGame = catchAsync(async (req, res, next) => {
-//   const backlog = await Backlog.findOneAndUpdate(
-//     {
-//       owner: req.user._id,
-//       "items._id": req.params.gameId
-//     },
-//     {
-//       $set: {
-//         "items.$.logType": req.body.logType
-//       }
-//     },
-//     {
-//       new: true,
-//       runValidators: true
-//     }
-//   );
-
-//   if (!backlog)
-//     return next(new AppError("No game with this ID waws found.", 404));
-
-//   res.status(200).json({ backlog });
-// });
-
-// exports.daleteGame = catchAsync(async (req, res, next) => {
-//   const backlog = await Backlog.findOneAndUpdate(
-//     {
-//       owner: req.user._id,
-//       "items._id": req.params.gameId
-//     },
-//     {
-//       $pull: {
-//         items: {
-//           _id: req.params.gameId
-//         }
-//       }
-//     },
-//     {
-//       new: true,
-//       runValidators: true
-//     }
-//   );
-
-//   if (!backlog)
-//     return next(new AppError("No game with this ID was found.", 404));
-
-//   res.status(200).json({ backlog });
-// });
 
 exports.createGame = catchAsync(async (req, res, next) => {
   const game = await Game.findOneAndUpdate(
