f71fbd1d4436a349de97ecfe71814ae7852022d2
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2020-04-01 12:40:51 +0200

    improve security

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 const compression = require("compression");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -8,10 +12,19 @@ const gameRouter = require("./routes/gameRoutes");
 
 const app = express();
 
+app.use(helmet());
+const limiter = rateLimit({
+  max: 500,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!"
+});
+app.use(limiter);
 app.use(cors());
 app.options("*", cors());
 // app.all("*", cors({ allowedHeaders: ["Content-Type", "Authorization"] }));
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
+app.use(mongoSanitize());
+app.use(xss());
 app.use(compression());
 
 app.use("/api/v1/users", userRouter);
